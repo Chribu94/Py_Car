42d131df488e78f195ca85caaf1f7cc8886f19d0
Christian Bunzeck <christian@bunzeck.de>
2019-08-23 22:36:53 +0200

    rework

--- Video_Server.py
+++ Video_Server.py
@@ -1,55 +1,11 @@
+from imutils import build_montages
 import numpy as np
+import imagezmq
+import imutils
 import cv2
-import socket
 
+imageHub = imagezmq.ImageHub()
 
-class VideoStreamingTest(object):
-    def __init__(self, host, port):
-
-        self.server_socket = socket.socket()
-        self.server_socket.bind((host, port))
-        self.server_socket.listen(0)
-        self.connection, self.client_address = self.server_socket.accept()
-        self.connection = self.connection.makefile('rb')
-        self.host_name = socket.gethostname()
-        self.host_ip = socket.gethostbyname(self.host_name)
-        self.streaming()
-
-    def streaming(self):
-
-        try:
-            print("Host: ", self.host_name + ' ' + self.host_ip)
-            print("Connection from: ", self.client_address)
-            print("Streaming...")
-            print("Press 'q' to exit")
-
-            # need bytes here
-            stream_bytes = b' '
-            while True:
-                stream_bytes += self.connection.read(1024)
-                first = stream_bytes.find(b'\xff\xd8')
-                last = stream_bytes.find(b'\xff\xd9')
-                if first != -1 and last != -1:
-                    jpg = stream_bytes[first:last + 2]
-                    stream_bytes = stream_bytes[last + 2:]
-                    image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-                    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                    blurred = cv2.GaussianBlur(gray, (9, 9), 1)
-                    edged = cv2.Canny(blurred, 40, 40)
-                    shape = (image.shape[1]*2,image.shape[0]*2)
-                    cv2.imshow('Raw', cv2.resize(image, shape))
-                    cv2.imshow('Blurred', cv2.resize(blurred, shape))
-                    cv2.imshow('Edged', cv2.resize(edged, shape))
-
-
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
-        finally:
-            self.connection.close()
-            self.server_socket.close()
-
-
-if __name__ == '__main__':
-    # host, port
-    h, p = "192.168.178.78", 8000
-    VideoStreamingTest(h, p)
+while True:
+    (rpiName, frame) = imageHub.recv_image()
+    imageHub.send_reply(b'OK')
--- car_client.py
+++ car_client.py
@@ -1,10 +1,7 @@
-import io
+from imutils.video import VideoStream
+import imagezmq
 import socket
-import struct
 import time
-import picamera
-import sys
-from time import sleep
 
 def prepServo(angle=40, pin=16):
     import RPi.GPIO as GPIO
@@ -20,47 +17,22 @@ def prepServo(angle=40, pin=16):
     GPIO.output(pin, False)
     servo.ChangeDutyCycle(0)
 
-class SplitFrames(object):
-    def __init__(self, connection):
-        self.connection = connection
-        self.stream = io.BytesIO()
-        self.count = 0
 
-    def write(self, buf):
-        if buf.startswith(b'\xff\xd8'):
-            # Start of new frame; send the old one's length
-            # then the data
-            size = self.stream.tell()
-            if size > 0:
-                self.connection.write(struct.pack('<L', size))
-                self.connection.flush()
-                self.stream.seek(0)
-                self.connection.write(self.stream.read(size))
-                self.count += 1
-                self.stream.seek(0)
-        self.stream.write(buf)
 
 prepServo()
 sleep(1)
 my_server = '192.168.178.78'
-res = (400, 320)
-client_socket = socket.socket()
-client_socket.connect((my_server, 8000))
-connection = client_socket.makefile('wb')
-try:
-    output = SplitFrames(connection)
-    with picamera.PiCamera(resolution=res, framerate=20) as camera:
-        time.sleep(1)
-        start = time.time()
-        camera.start_recording(output, format='mjpeg')
-        camera.wait_recording(60*60)
-        camera.stop_recording()
-        # Write the terminating 0-length to the connection to let the
-        # server know we're done
-        connection.write(struct.pack('<L', 0))
-finally:
-    finish = time.time()
-    print('Sent %d images in %d seconds at %.2ffps' % (
-    output.count, finish-start, output.count / (finish-start)))
-    connection.close()
-    client_socket.close()
+sender = imagezmq.ImageSender(connect_to="tcp://192.168.178.78:5555")
+
+rpiName = socket.gethostname()
+vs = VideoStream(usePiCamera=True, resolution=(1280, 720)).start()
+
+time.sleep(1.0)
+ 
+while True:
+    frame = vs.read()
+    sender.send_image(rpiName, frame)
+    cv2.imshow("Raw", frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
